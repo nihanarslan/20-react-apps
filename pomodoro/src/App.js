4e12f3c387b7871fba825316911dc9c8bcc1653c
Nihan Arslan <nihanarslan@outlook.com>
2023-10-11 23:16:17 +0300

    Added new features to app

--- pomodoro/src/App.js
+++ pomodoro/src/App.js
@@ -1,5 +1,7 @@
 import React, { useRef, useState } from "react";
 import "./App.css";
+import { MdReplayCircleFilled } from "react-icons/md";
+import { AiFillPlayCircle, AiFillPauseCircle } from "react-icons/ai";
 
 function padTime(time) {
   return time.toString().padStart(2, "0");
@@ -9,12 +11,13 @@ function App() {
   const [title, setTitle] = useState("Let the countdown begin!!!");
   const [timeLeft, setTimeLeft] = useState(25 * 60);
   const [isRunning, setIsRunning] = useState(false);
+  const [background, setBackGround] = useState("#f84935");
   const intervalRef = useRef(null);
 
   function startTimer() {
     if (intervalRef.current != null) return;
 
-    setTitle("You're doing great");
+    setTitle("You're doing great!");
     setIsRunning(true);
 
     intervalRef.current = setInterval(() => {
@@ -45,11 +48,38 @@ function App() {
     setIsRunning(false);
   }
 
+  function setPomodoroTimer() {
+    setTitle("Let the countdown begin!!!");
+    setTimeLeft(25 * 60);
+    setBackGround("#f84935");
+    stopTimer();
+  }
+
+  function setShortTimer() {
+    setTitle("Time for a break!");
+    setTimeLeft(5 * 60);
+    setBackGround("#4A678A");
+    stopTimer();
+  }
+
+  function setLongTimer() {
+    setTitle("Time for a break!");
+    setTimeLeft(15 * 60);
+    setBackGround("#768A4A");
+    stopTimer();
+  }
+
   const minutes = padTime(Math.floor(timeLeft / 60));
   const seconds = padTime(timeLeft - minutes * 60);
 
   return (
-    <div className="app">
+    <div className="app" style={{ background: background }}>
+      <div className="buttons">
+        <button onClick={setPomodoroTimer}>Pomodoro</button>
+        <button onClick={setShortTimer}>Short Break</button>
+        <button onClick={setLongTimer}>Long Break</button>
+      </div>
+
       <h2>{title}</h2>
 
       <div className="timer">
@@ -58,10 +88,14 @@ function App() {
         <span>{seconds}</span>
       </div>
 
-      <div className="buttons">
-        {!isRunning && <button onClick={startTimer}>Start</button>}
-        {isRunning && <button onClick={stopTimer}>Stop</button>}
-        <button onClick={resetTimer}>Reset</button>
+      <div className="icon_group">
+        {!isRunning && (
+          <AiFillPlayCircle className="icon" onClick={startTimer} />
+        )}
+        {isRunning && (
+          <AiFillPauseCircle className="icon" onClick={stopTimer} />
+        )}
+        <MdReplayCircleFilled className="icon" onClick={resetTimer} />
       </div>
     </div>
   );
