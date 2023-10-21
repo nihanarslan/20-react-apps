1892c547fe819bcfafe69ab26e5c7cfa1036b45e
Nihan Arslan <nihanarslan@outlook.com>
2023-10-22 00:36:42 +0300

    Paper-Rock-Scissors project is created and some functions added

---
+++ paper-rock-scissors/src/App.js
@@ -0,0 +1,98 @@
+import React, { useEffect, useState } from "react";
+import Rock from "./icons/Rock";
+import Paper from "./icons/Paper";
+import Scissors from "./icons/Scissors";
+import "./App.css";
+
+const choices = [
+  { id: 1, name: "rock", component: Rock },
+  { id: 2, name: "paper", component: Paper },
+  { id: 3, name: "scissors", component: Scissors },
+];
+export default function App() {
+  const [wins, setWins] = useState(0);
+  const [losses, setLosses] = useState(0);
+  const [userChoice, setUserChoice] = useState(null);
+  const [computerChoice, setComputerChoice] = useState(null);
+  const [gameState, setGameState] = useState(null); //win, lose, draw
+
+  function handleUserChoice(choice) {
+    const chosenChoice = choices.find((x) => x.id === choice);
+    setUserChoice(chosenChoice);
+
+    //determine the winner
+    setGameState("win");
+  }
+
+  useEffect(() => {
+    const randomChoice = choices[Math.floor(Math.random() * choices.length)];
+    setComputerChoice(randomChoice);
+  }, []);
+
+  function renderComponent(choice) {
+    const Component = choice.component; //Paper,Rock,Scissors
+    return <Component />;
+  }
+
+  return (
+    <div className="app">
+      {/* information goes here */}
+      <div className="info">
+        <h2>Rock. Paper. Scissors</h2>
+
+        {/* wins vs losses stats */}
+        <div className="wins-losses">
+          <div className="wins">
+            <span className="number">{wins}</span>
+            <span className="text">{wins <= 1 ? "Win" : "Wins"}</span>
+          </div>
+
+          <div className="losses">
+            <span className="number">{losses}</span>
+            <span className="text">{losses <= 1 ? "Loss" : "Losses"}</span>
+          </div>
+        </div>
+      </div>
+
+      {/* the popup to show win/lose/draw */}
+      {gameState && (
+        <div className={`game-state ${gameState}`}>
+          <div>
+            <div className="game-state-content">
+              <p>{renderComponent(userChoice)}</p>
+              <p>you won!</p>
+              <p>{renderComponent(computerChoice)}</p>
+            </div>
+          </div>
+        </div>
+      )}
+
+      <div className="choices">
+        {/* choices captions */}
+        <div>You</div>
+        <div />
+        <div>Computer</div>
+
+        {/* buttons for my choice */}
+        <div>
+          <button className="rock" onClick={() => handleUserChoice(1)}>
+            <Rock />
+          </button>
+          <button className="paper" onClick={() => handleUserChoice(2)}>
+            <Paper />
+          </button>
+          <button className="scissors" onClick={() => handleUserChoice(3)}>
+            <Scissors />
+          </button>
+        </div>
+
+        <div className="vs">vs</div>
+
+        {/* show the computer's choice */}
+        <div>
+          <button className="computer-choice">?</button>
+        </div>
+      </div>
+    </div>
+  );
+}
