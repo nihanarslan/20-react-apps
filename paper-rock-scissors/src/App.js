b1ae1fc6b074bddfe721b68c39b954af52f14ef1
Nihan Arslan <nihanarslan@outlook.com>
2023-10-22 21:54:10 +0300

    rock-paper-scissors application is completed

--- paper-rock-scissors/src/App.js
+++ paper-rock-scissors/src/App.js
@@ -5,9 +5,9 @@ import Scissors from "./icons/Scissors";
 import "./App.css";
 
 const choices = [
-  { id: 1, name: "rock", component: Rock },
-  { id: 2, name: "paper", component: Paper },
-  { id: 3, name: "scissors", component: Scissors },
+  { id: 1, name: "rock", component: Rock, losesTo: 2 },
+  { id: 2, name: "paper", component: Paper, losesTo: 3 },
+  { id: 3, name: "scissors", component: Scissors, losesTo: 1 },
 ];
 export default function App() {
   const [wins, setWins] = useState(0);
@@ -16,22 +16,36 @@ export default function App() {
   const [computerChoice, setComputerChoice] = useState(null);
   const [gameState, setGameState] = useState(null); //win, lose, draw
 
+  useEffect(() => {
+    reStartGame();
+  }, []);
+
   function handleUserChoice(choice) {
     const chosenChoice = choices.find((x) => x.id === choice);
     setUserChoice(chosenChoice);
 
     //determine the winner
-    setGameState("win");
+    if (chosenChoice.losesTo === computerChoice.id) {
+      setLosses((losses) => losses + 1);
+      setGameState("lose");
+    } else if (chosenChoice.id === computerChoice.losesTo) {
+      setWins((wins) => wins + 1);
+      setGameState("win");
+    } else if (computerChoice.id === chosenChoice.id) {
+      setGameState("draw");
+    }
   }
-
-  useEffect(() => {
-    const randomChoice = choices[Math.floor(Math.random() * choices.length)];
-    setComputerChoice(randomChoice);
-  }, []);
 
   function renderComponent(choice) {
     const Component = choice.component; //Paper,Rock,Scissors
     return <Component />;
+  }
+
+  function reStartGame() {
+    setGameState(null);
+    setUserChoice(null);
+    const randomChoice = choices[Math.floor(Math.random() * choices.length)];
+    setComputerChoice(randomChoice);
   }
 
   return (
@@ -57,12 +71,17 @@ export default function App() {
       {/* the popup to show win/lose/draw */}
       {gameState && (
         <div className={`game-state ${gameState}`}>
-          <div>
+          <div className="game-state-column">
             <div className="game-state-content">
               <p>{renderComponent(userChoice)}</p>
-              <p>you won!</p>
+
+              {gameState === "win" && <p>Congrats! You won!</p>}
+              {gameState === "lose" && <p>Sorry! You lost!</p>}
+              {gameState === "draw" && <p>You drew!</p>}
+
               <p>{renderComponent(computerChoice)}</p>
             </div>
+            <button onClick={reStartGame}>Play Again</button>
           </div>
         </div>
       )}
